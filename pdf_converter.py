6a11121feeb3776d85feb179931db5958b1a8c91
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2022-12-16 02:01:35 +0100

    Refactoring and cleanup of code

--- pdf_converter.py
+++ pdf_converter.py
@@ -1,16 +1,34 @@
 from pdf2docx import Converter
 
-print("PDF2DOCX by Solomon Taiwo\n" + "-" * 50 + "\nThis program converts a PDF to a docx file.\n")
+print("\n- " + "PDF Converter: convert a PDF to a docx file" + " -\n")
 
 try:
-    pdf_file = input("Insert the name of the pdf file: ") # Ask the user for the name of the file
-    pdf_file= pdf_file + ".pdf"                           # Add .pdf extension to the filename
-    docx_file = "converted_" + pdf_file[0:-4] + ".docx"   # Add "converted" and ".docx" to converted filename
-    file = Converter(pdf_file)                            # Open pdf file to convert
-    file.convert(docx_file)                               # Convert file to docx
-    file.close()                                          # Close file
-    print("Done, your file has been converted! Closing program...")
+    # Ask the user for the name of the file
+    pdf_file = input("Insert the name of the pdf file: ")
+
+    print()
+
+    # Add .pdf extension to the filename
+    pdf_file = pdf_file + ".pdf"
+
+    # Add "converted" and ".docx" to converted filename
+    docx_file = "converted_" + pdf_file[0:-4] + ".docx"
+
+    # Open pdf file to convert
+    file = Converter(pdf_file)
+
+    # Convert file to docx
+    file.convert(docx_file)
+
+    # Close file
+    file.close()
+
+    print("\nDone, your file has been converted! Closing program...\n")
+
+# Print error if file is not found
 except IOError as i:
     print(i)
+
+# Print error if convertion can't be done
 except Exception as e:
     print(e)
